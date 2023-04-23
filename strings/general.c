21c3fd3f9dc90e1732c50e2dc8a230511cd82463
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-04-23 14:11:29 +0100

    added 2 function for the strings
    sport function for strings in the sports type words
    and general for general words

---
+++ strings/general.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+char* general(int a, char* gen) {
+    int i;
+    char bit[][20] = {
+        "Morocco", "Football", "Engineer", "Marrakech", "Gaming",
+        "Speed", "School", "Armor", "GYM", "Maths", "Physics",
+        "Pool", "Propaganda", "Manipulation", "Famous", "Money",
+        "Cake", "Food", "Cristiano", "Basketball", "Chess",
+        "Mohamed", "Winner", "Success", "Legendary"
+    };
+   
+    for (i = 0; i <1 ; i++) {
+        strcpy(gen, bit[a]); // copy string from bit[i] to gen
+        gen += strlen(bit[a]); // advance gen pointer to end of copied string
+    }
+    *gen = '\0'; // add null terminator to the end of the string
+    
+    return gen;
+}
---
+++ strings/sport.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+char* sport(int a, char* gen) {
+    int i;
+    char bit[][20] = {
+        "Goal", "Basket", "Helmet", "Volley", "Soccer", "Tennis", 
+        "Racket", "Swim", "Skate", "Jump", "Dive", "Run", "Cycle", "Boxing", 
+        "Fitness", "Golf", "Hockey", "Sprint", "Surf", "Yoga", "Basketball", 
+        "Football", "Baseball", "Weightlifting", "Climbing", "Archery", 
+        "Badminton", "Table Tennis", "Fencing", "Rowing"
+    };
+   
+    for (i = 0; i <1 ; i++) {
+        strcpy(gen, bit[a]); // copy string from bit[i] to gen
+        gen += strlen(bit[a]); // advance gen pointer to end of copied string
+    }
+    *gen = '\0'; // add null terminator to the end of the string
+    
+    return gen;
+}
