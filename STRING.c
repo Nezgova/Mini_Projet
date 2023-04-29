55f251ee02415c4b239d7ad4ad595c8b4a198b78
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-04-29 17:53:24 +0100

    word function
    completed the word function with all the themes and the random number

--- STRING.c
+++ STRING.c
@@ -1,1 +1,51 @@
 #include "import/strings.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUM 1000
+
+char* word_function(int a) {
+    int randomNum;
+    char* word = malloc(20 * sizeof(char));
+    srand(time(NULL));
+    randomNum = rand() % 50 + 1;
+  
+    FILE *file;
+    char file_char[MAX_NUM];
+    
+    switch (a) {
+        case 1:
+            file = fopen("textfiles/genral.txt", "r");
+            break;
+        case 2:
+            file = fopen("textfiles/sport.txt", "r");
+            break;
+        case 3:
+            file = fopen("textfiles/movies.txt", "r");
+            break;
+        case 4:
+            file = fopen("textfiles/music.txt", "r");
+            break;
+        default:
+            break;
+    }
+    
+    int currentLine = 1;
+    while (fgets(file_char, MAX_NUM, file) != NULL) {
+        if (currentLine == randomNum) {
+            strcpy(word, file_char);
+            break;
+        }
+        currentLine++;
+    }
+
+    if (currentLine > randomNum) {
+        printf("Line %d does not exist in the file.\n", randomNum);
+    }
+
+    fclose(file);
+
+    return word;
+}
