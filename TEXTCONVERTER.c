9b320381c60a68969097225f55277ba605faa8b5
taha-ammour <122755276+taha-ammour@users.noreply.github.com>
2023-04-30 13:59:13 +0100

    Update TEXTCONVERTER.c

--- TEXTCONVERTER.c
+++ TEXTCONVERTER.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #include "import/textcoverter.h"
 
-int character_counter(char *chara){
+int character_counter(char *chara)
+{
     int count = 0;
     for (int i = 0; chara[i] != '\0'; i++)
     {
-        count ++;
+        count++;
     }
     return count;
 }
@@ -20,15 +21,20 @@ void num_to_word(char *find, char *secret)
     find[strlen(secret)] = '\0';
 }
 
-int check_already_guessed(char guess, char* find, int length, char* guessedl) {
-    for (int i = 0; i < length; i++) {
-        if (guess == find[i]) {
+int check_already_guessed(char guess, char *find, int length, char *guessedl)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (guess == find[i])
+        {
             printf("You already guessed that letter.\n");
             return 1;
         }
     }
-    for (int i = 0; i < strlen(guessedl); i++) {
-        if (guess == guessedl[i]) {
+    for (int i = 0; i < strlen(guessedl); i++)
+    {
+        if (guess == guessedl[i])
+        {
             printf("You already guessed the letter %c. Try another letter.\n", guess);
             return 1;
         }
@@ -38,18 +44,21 @@ int check_already_guessed(char guess, char* find, int length, char* guessedl) {
     return 0;
 }
 
-
-int check_guess(char guess, char* secret, char* find, int* nmcg) {
+int check_guess(char guess, char *secret, char *find, int *nmcg)
+{
     int found = 0;
-    for (int i = 0; i < strlen(secret); i++) {
-        if (guess == secret[i]) {
+    for (int i = 0; i < strlen(secret); i++)
+    {
+        if (guess == secret[i])
+        {
             find[i] = guess;
             (*nmcg)++;
             found = 1;
         }
     }
 
-    if (!found) {
+    if (!found)
+    {
         printf("Sorry, the letter %c is not in the word.\n", guess);
         return 0;
     }
@@ -58,42 +67,65 @@ int check_guess(char guess, char* secret, char* find, int* nmcg) {
     return 1;
 }
 
-void print_game_status(char* find, int ng) {
+void print_game_status(char *find, int ng)
+{
     printf("\n%s\n", find);
     printf("%d guesses remaining.\n", ng);
 }
 
-int play_hangman(char* secret, Difficulty diff) {
-    int ng = 6;
+int play_hangman(char *secret, Difficulty diff)
+{
+    int ng = 9;
+    switch (diff)
+    {
+    case EASY:
+        ng = 9;
+        break;
+    case MEDIUM:
+        ng = 6;
+        break;
+    case HARD:
+        ng = 3;
+        break;
+    default:
+        ng = 9;
+        break;
+    }
+
     int nmcg = 0;
     int secret_length = strlen(secret);
-    char find[secret_length +1];
+    char find[secret_length + 1];
     char guessedl[27] = {0};
     char guess;
     int game_over = 0;
 
     num_to_word(find, secret);
 
-    while (ng > 0 && !game_over) {
+    while (ng > 0 && !game_over)
+    {
         print_game_status(find, ng);
 
         printf("Guess a letter: ");
         scanf(" %c", &guess);
 
-        if (check_already_guessed(guess, find, secret_length, guessedl)) {
+        if (check_already_guessed(guess, find, secret_length, guessedl))
+        {
             continue;
         }
 
-        if (!check_guess(guess, secret, find, &nmcg)) {
+        if (!check_guess(guess, secret, find, &nmcg))
+        {
             ng--;
         }
 
-        if (nmcg == secret_length) {
+        if (nmcg == secret_length)
+        {
             printf("\n%s\n", find);
             printf("Congratulations, you guessed the word!\n");
             return 1;
         }
-        else if (ng == 0) {
+        else if (ng == 0)
+        {
             printf("Sorry, you ran out of guesses. The word was %s.\n", secret);
             return 0;
         }
