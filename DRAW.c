b5574f47872366fed7c53dbe9561af33271db508
Rania-Elhezzam <128979605+Rania-Elhezzam@users.noreply.github.com>
2023-05-02 14:03:52 +0100

    Modification
    cette fonction sert à dessiner le hangman selon le niveau difficulté sélectionné par l’utilisateur
    la fonction trait 3 cas :
    (easy):nombre de tentatives=7
    (simple):nombre de tentatives=6
    (difficult):nombre de tentatives=5

--- DRAW.c
+++ DRAW.c
@@ -1,1 +1,57 @@
 #include "import/draw.h"
+
+enum (easy,simple,difficulte) difficulty;
+
+
+
+void displayHangman(int numTries,difficulty diff ) {
+    
+    switch (diff)
+    {
+    case (easy) :
+    printf("   ________\n");
+    printf("   |/     |\n");
+    printf("   |      %c \n", (numTries >= 1) ? 'O' : ' ');
+    printf("   |     %c%c%c\n", (numTries >= 3) ? '/' : ' ', (numTries >= 2) ? '|' : ' ', (numTries >= 4) ? '\\' : ' ');
+    printf("   |      %c \n", (numTries >= 5) ? '|' : ' ');
+    printf("   |     %c %c\n", (numTries >= 6) ? '/' : ' ', (numTries >= 7) ? '\\' : ' ');
+    printf("   |    \n");
+    printf("   |    \n");
+    printf("___|___________\n");
+    
+    
+    
+        break;
+    
+
+    case (simple) :
+    printf("   ________\n");
+    printf("   |/     |\n");
+    printf("   |      %c \n", (numTries >= 1) ? 'O' : ' ');
+    printf("   |     %c%c%c\n", (numTries >= 3) ? '/' : ' ', (numTries >= 2) ? '|' : ' ', (numTries >= 3) ? '\\' : ' ');
+    printf("   |      %c \n", (numTries >= 4) ? '|' : ' ');
+    printf("   |     %c %c\n", (numTries >= 5) ? '/' : ' ', (numTries >= 6) ? '\\' : ' ');
+    printf("   |    \n");
+    printf("   |    \n");
+    printf("___|___________\n");
+    
+    
+    
+        break;
+    
+    case (difficult) :
+    printf("   ________\n");
+    printf("   |/     |\n");
+    printf("   |      %c \n", (numTries >= 1) ? 'O' : ' ');
+    printf("   |     %c%c%c\n", (numTries >= 2) ? '/' : ' ', (numTries >= 2) ? '|' : ' ', (numTries >= 2) ? '\\' : ' ');
+    printf("   |      %c \n", (numTries >= 3) ? '|' : ' ');
+    printf("   |     %c %c\n", (numTries >= 4) ? '/' : ' ', (numTries >= 5) ? '\\' : ' ');
+    printf("   |    \n");
+    printf("   |    \n");
+    printf("___|___________\n");
+    
+    
+    
+        break;
+    
+}
